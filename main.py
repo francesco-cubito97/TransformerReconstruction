4775f7d01d2709c83368b59ef0eb42a77ddb5387
Francesco Cubito <50382393+francesco-cubito97@users.noreply.github.com>
2022-09-15 10:42:03 +0200

    Update main.py
    Updated input and hidden layers

--- main.py
+++ main.py
@@ -85,9 +85,9 @@ def parseArguments():
                         "hidden_size / num_attention_heads should be in integer.")
     parser.add_argument("--intermediate_size", default=-1, type=int, required=False, 
                         help="Update model config if given.")
-    parser.add_argument("--input_feat_dim", default='1025,256,32', type=str, 
+    parser.add_argument("--input_feat_dim", default='1025,64', type=str, 
                         help="Input image feature dimensions")          
-    parser.add_argument("--hidden_feat_dim", default='512,128,16', type=str, 
+    parser.add_argument("--hidden_feat_dim", default='256,16', type=str, 
                         help="Hidden image freature dimensions")   
 
     parser.add_argument("--multiscale_inference", default=False, type=bool)
